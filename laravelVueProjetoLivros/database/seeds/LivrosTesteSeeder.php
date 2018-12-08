9839573e747fac726035b195462937a4c928cc2d
Cleber Farinazzo Junior <cleber.farinazzo.junior@gmail.com>
2018-12-07 23:15:25 -0200

    Seeders corrigidos

--- laravelVueProjetoLivros/database/seeds/LivrosTesteSeeder.php
+++ laravelVueProjetoLivros/database/seeds/LivrosTesteSeeder.php
@@ -9,46 +9,57 @@ class LivrosTesteSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
+    public function run() {
 
 
-        DB::table('livros')->insert(
-            [
-                'isbn' => '9788580416824',
-                'titulo' => 'JARDINS DA LUA',
-                'autor' => 'Steven Erikson',      
-                'paginas' => '608',
-                'anoLancamento' => '2017',    
-                'editora' => 'Arqueiro',
-                'edicao' => '1',
-                'genero' => 'Grimdark Fantasy',
-                'descricao' => 'descricao exemplo'
-            ],
-            [
-                'isbn' => '9788580411874',
-                'titulo' => 'O Poder da Espada',
-                'autor' => 'Joe Abercrombie',
-                'paginas' => '480',
-                'anoLancamento' => '2014',
-                'editora' => 'Arqueiro',
-                'edicao' => '2',
-                'genero' => 'Grimdark Fantasy',
-                'descricao' => 'descricao exemplo'
+        DB::table('livros')->insert([
+            'isbn' => '9788580416824',
+            'titulo' => 'JARDINS DA LUA',
+            'autor' => 'Steven Erikson',      
+            'paginas' => '608',
+            'anoLancamento' => '2017',    
+            'editora' => 'Arqueiro',
+            'edicao' => '1',
+            'genero' => 'Grimdark Fantasy',
+            'descricao' => 'descricao exemplo',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
 
-            ],
-            [
-                'isbn' => '9788567296340',
-                'titulo' => 'A Filha do Império',
-                'autor' => 'Raymond E. Feist',
-                'paginas' => '464',
-                'anoLancamento' => '2016',
-                'editora' => 'Arqueiro',
-                'edicao' => '1',
-                'genero' => 'Fantasy',
-                'descricao' => 'descricao exemplo'
-            ]
-        );
+
+        DB::table('livros')->insert([
+            'isbn' => '9788580411874',
+            'titulo' => 'O Poder da Espada',
+            'autor' => 'Joe Abercrombie',
+            'paginas' => '480',
+            'anoLancamento' => '2014',
+            'editora' => 'Arqueiro',
+            'edicao' => '2',
+            'genero' => 'Grimdark Fantasy',
+            'descricao' => 'descricao exemplo',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+
+        ]);
+
+
+
+        DB::table('livros')->insert([
+            'isbn' => '9788567296340',
+            'titulo' => 'A Filha do Império',
+            'autor' => 'Raymond E. Feist',
+            'paginas' => '464',
+            'anoLancamento' => '2016',
+            'editora' => 'Arqueiro',
+            'edicao' => '1',
+            'genero' => 'Fantasy',
+            'descricao' => 'descricao exemplo',
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+         ]);
+
+
+        
     
         
 
