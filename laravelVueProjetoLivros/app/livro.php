83851ed21fea980f7e333fb486a0a367b8d41258
Cleber Farinazzo Junior <cleber.farinazzo.junior@gmail.com>
2018-12-08 23:20:09 -0200

    Começando o trabalho com as rotas

--- laravelVueProjetoLivros/app/livro.php
+++ laravelVueProjetoLivros/app/livro.php
@@ -4,7 +4,7 @@ namespace laravelVueProjetoLivros;
 
 use Illuminate\Database\Eloquent\Model;
 
-class livro extends Model
+class Livro extends Model
 {
     protected $fillable = array('isbn','titulo','autor', 'paginas', 'anoLancamento', 'editora', 'edicao', 'genero', 'descricao');
 
--- laravelVueProjetoLivros/routes/web.php
+++ laravelVueProjetoLivros/routes/web.php
@@ -11,6 +11,30 @@
 |
 */
 
+
+
+//Home da aplicação
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//Recursos
+Route::resource('livros', 'LivroController');
+
+
+
+
+
+
+//Captura de falhas
+Route::fallback(function () {
+    //TODO: falta criar a view que vou chamar aqui
+    //Direcionar para alguma página que seja handler de erros gerais,
+    //como 404 e semelhantes
+});
+
+
+
+
+
