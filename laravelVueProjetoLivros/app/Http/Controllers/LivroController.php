a19207909fcf04dcb44a244288eaef4fad12f9d2
Cleber Farinazzo Junior <cleber.farinazzo.junior@gmail.com>
2018-12-08 23:47:48 -0200

    Apenas inserção de comentários no LivroController

--- laravelVueProjetoLivros/app/Http/Controllers/LivroController.php
+++ laravelVueProjetoLivros/app/Http/Controllers/LivroController.php
@@ -7,57 +7,54 @@ use Illuminate\Http\Request;
 
 class LivroController extends Controller
 {
-
-
-
+    /**
+     * Exibe a lista de itens salvos no banco
+     */
     public function index(){
         //TODO
     }
 
-
-
-
+    /**
+     * Retorna a página que mostra o formulário
+     * para cadastro de um novo item
+     */
     public function create(){
-        //TODO
+        //TODO 
     }
 
-
-
-
+    /**
+     * Salva o item no banco
+     */
     public function store(Request $request){
         //TODO
     }
 
-
-
-
+    /**
+     * Exibe o item solicitado
+     */
     public function show(Livro $livro){
         //TODO
     }
 
-
-
-
+    /**
+     * Exibe o formulário para edição de um item
+     */
     public function edit(Livro $livro){
         //TODO
     }
 
-
-
-
-
-    public function update(Request $request, Livro $livro){
+    /**
+     * Atualiza o item no banco 
+     */
+    public function update(Request $request, Livro $livro) {
         //TODO
     }
 
-
-
-
-    public function destroy(Livro $livro){
+    /**
+     *   Apaga o recurso do banco
+     */
+    public function destroy(Livro $livro)
+    {
         //TODO
     }
-
-
-
-    
 }
