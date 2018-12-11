bbd09c396863ac5c306b581b542fcc25175f7788
Cleber Farinazzo Junior <cleber.farinazzo.junior@gmail.com>
2018-12-10 22:55:08 -0200

    Pequenas correções e criaçao dos arquivos (ainda vazios) dos componentes vue

---
+++ laravelVueProjetoLivros/resources/js/components/livroComponents/livrosFormComponent.vue
@@ -0,0 +1,1 @@
+<!-- TODO: criar componente, declarar no app.js e chamar na view -->
--- laravelVueProjetoLivros/resources/views/partials/header.blade.php
+++ laravelVueProjetoLivros/resources/views/partials/header.blade.php
@@ -1,3 +1,3 @@
-<!-- TODO: escrever esse blade, criando o cabeçalo com uma barra de navegação -->
-<!-- Deve incluir também o menu de cadastr/login e logout -->
+<!-- TODO: escrever esse blade, criando o cabeçalho com uma barra de navegação -->
+<!-- Essa barra de navegação deverá incluir também o menu de cadastro/login e logout -->
 Esse é o cabeçalho
